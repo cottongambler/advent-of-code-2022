250c02f9af3d099b6f208891f728ed79460282f6
Cotton <gamblercotton@gmail.com>
2022-12-02 14:23:59 +0100

    Day 2 part 2 completed

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -3,7 +3,8 @@ import { Day } from "../day";
 const enum RockPaperScissor {
     Rock = "rock",
     Paper = "paper",
-    Scissor = "scissor"
+    Scissor = "scissor",
+    Invalid = "invalid"
 };
 
 const enum GameState {
@@ -11,12 +12,6 @@ const enum GameState {
     Draw = "draw",
     Lose = "lose",
     Invalid = "invalid"
-};
-
-const playerInputMapper = {
-    X: RockPaperScissor.Rock,
-    Y: RockPaperScissor.Paper,
-    Z: RockPaperScissor.Scissor
 };
 
 const opponentInputMapper = {
@@ -51,9 +46,8 @@ class Day2 extends Day {
         .split("\n")
         .forEach(round => {
             const [opponentInput, playerInput] = round.split(" ");
-            const player = playerInputMapper[playerInput as keyof typeof playerInputMapper];
-            const opponent = opponentInputMapper[opponentInput as keyof typeof opponentInputMapper];
-            const roundScore = this.roundScore(player, opponent);
+
+            const roundScore = this.roundScorePart1(playerInput, opponentInput);
             scores.push(roundScore);
         });
 
@@ -61,15 +55,35 @@ class Day2 extends Day {
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const scores: number[] = [];
+
+        input
+        .split("\n")
+        .forEach(round => {
+            const [opponentInput, roundResultInput] = round.split(" ");
+
+            const roundScore = this.roundScorePart2(opponentInput, roundResultInput);
+            scores.push(roundScore);
+        });
+
+        return this.getTotalScore(scores).toString();
     }
 
-    roundScore(player: RockPaperScissor, opponent: RockPaperScissor): number {
-        const roundResult = this.playRound(player, opponent);
-        return gameScoreMapper[roundResult] + playerScoreMapper[player];
+    roundScorePart1(playerInput: string, opponentInput: string): number {
+        const playerInputMapperPart1 = {
+            X: RockPaperScissor.Rock,
+            Y: RockPaperScissor.Paper,
+            Z: RockPaperScissor.Scissor
+        };
+
+        const player = playerInputMapperPart1[playerInput as keyof typeof playerInputMapperPart1];
+        const opponent = opponentInputMapper[opponentInput as keyof typeof opponentInputMapper];
+
+        const roundResult = this.playRoundPart1(player, opponent);
+        return gameScoreMapper[roundResult] + playerScoreMapper[player as keyof typeof playerScoreMapper];
     }
 
-    playRound(player: RockPaperScissor, opponent: RockPaperScissor): GameState {
+    playRoundPart1(player: RockPaperScissor, opponent: RockPaperScissor): GameState {
         if (player === RockPaperScissor.Rock && opponent === RockPaperScissor.Rock) return GameState.Draw;
         if (player === RockPaperScissor.Paper && opponent === RockPaperScissor.Paper) return GameState.Draw;
         if (player === RockPaperScissor.Scissor && opponent === RockPaperScissor.Scissor) return GameState.Draw;
@@ -83,6 +97,36 @@ class Day2 extends Day {
         if (player === RockPaperScissor.Scissor && opponent === RockPaperScissor.Paper) return GameState.Lose;
 
         return GameState.Invalid;
+    }
+
+    roundScorePart2(opponentInput: string, roundResultInput: string): number {
+        const roundResultInputMapperPart2 = {
+            X: GameState.Lose,
+            Y: GameState.Draw,
+            Z: GameState.Win
+        };
+
+        const opponent = opponentInputMapper[opponentInput as keyof typeof opponentInputMapper];
+        const roundResult = roundResultInputMapperPart2[roundResultInput as keyof typeof roundResultInputMapperPart2];
+        const player = this.getPlayerMovePart2(opponent, roundResult);
+
+        return gameScoreMapper[roundResult] + playerScoreMapper[player as keyof typeof playerScoreMapper];
+    }
+
+    getPlayerMovePart2(opponent: RockPaperScissor, roundResult: GameState): RockPaperScissor {
+        if (opponent === RockPaperScissor.Rock && roundResult === GameState.Win) return RockPaperScissor.Paper;
+        if (opponent === RockPaperScissor.Rock && roundResult === GameState.Draw) return RockPaperScissor.Rock;
+        if (opponent === RockPaperScissor.Rock && roundResult === GameState.Lose) return RockPaperScissor.Scissor;
+
+        if (opponent === RockPaperScissor.Paper && roundResult === GameState.Win) return RockPaperScissor.Scissor;
+        if (opponent === RockPaperScissor.Paper && roundResult === GameState.Draw) return RockPaperScissor.Paper;
+        if (opponent === RockPaperScissor.Paper && roundResult === GameState.Lose) return RockPaperScissor.Rock;
+
+        if (opponent === RockPaperScissor.Scissor && roundResult === GameState.Win) return RockPaperScissor.Rock;
+        if (opponent === RockPaperScissor.Scissor && roundResult === GameState.Draw) return RockPaperScissor.Scissor;
+        if (opponent === RockPaperScissor.Scissor && roundResult === GameState.Lose) return RockPaperScissor.Paper;
+
+        return RockPaperScissor.Invalid;
     }
 
     getTotalScore(scores: number[]): number {
