111d439c63858e4dea90b0ebbd6fe87604044e4d
Cotton <gamblercotton@gmail.com>
2022-12-03 15:20:48 +0100

    Day 3 part 2 completed

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -1,24 +1,24 @@
 import { Day } from "../day";
 
 class Day3 extends Day {
+    characters: string[];
 
     constructor(){
         super(3);
+        this.characters = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
     }
 
     solveForPartOne(input: string): string {
-        const characters: string[] = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
         const priorities: number[] = [];
 
-        input.split('\n').forEach(ruckSack => {
-            const firstCompartment = ruckSack.split('').slice(0, ruckSack.length / 2);
-            const secondCompartment = ruckSack.split('').slice(ruckSack.length / 2);
+        input.split('\n').forEach(rucksack => {
+            const firstCompartment = rucksack.split('').slice(0, rucksack.length / 2);
+            const secondCompartment = rucksack.split('').slice(rucksack.length / 2);
 
             const sharedItemIndex = firstCompartment.findIndex(item => secondCompartment.includes(item));
             const sharedItem = firstCompartment[sharedItemIndex];
 
-            const sharedItemPriority = characters.findIndex(char => char === sharedItem);
-
+            const sharedItemPriority = this.getItemPriority(sharedItem);
             priorities.push(sharedItemPriority);
         });
 
@@ -26,7 +26,29 @@ class Day3 extends Day {
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const priorities: number[] = [];
+        const rucksacks = input.split('\n');
+        const groupSize = 3;
+
+        for (let i = 0; i < rucksacks.length; i += groupSize) {
+            const [firstRucksack, secondRucksack, thirdRucksack] = rucksacks.slice(i, i + groupSize);
+            
+            const sharedItemIndex = firstRucksack.split('')
+                .findIndex(item => 
+                    secondRucksack.split('').includes(item) 
+                    && thirdRucksack.split('').includes(item)
+                );
+            const sharedItem = firstRucksack[sharedItemIndex];
+
+            const sharedItemPriority = this.getItemPriority(sharedItem);
+            priorities.push(sharedItemPriority);
+        }
+
+        return this.sumPriorities(priorities).toString();
+    }
+
+    getItemPriority(item: string): number {
+        return this.characters.findIndex(char => char === item);
     }
 
     sumPriorities(priorities: number[]): number {
