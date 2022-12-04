63144acd552c3b484dab600d1e83bf53836dc20f
Cotton <gamblercotton@gmail.com>
2022-12-04 15:23:00 +0100

    Day 4 part 2 completed

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -8,30 +8,43 @@ class Day4 extends Day {
 
     solveForPartOne(input: string): string {
         const zonePairs: string[] = input.split('\n');
-        let sumFullRanges = 0;
+        let sumFullOverlappingRanges = 0;
 
         zonePairs.forEach(zones => {
             const [zoneOne, zoneTwo] = zones.split(',');
-            const [zoneOneStart, zoneOneEnd] = zoneOne.split('-').map(id => Number(id));
-            const [zoneTwoStart, zoneTwoEnd] = zoneTwo.split('-').map(id => Number(id));
+            const arrZoneOne = this.getZoneArray(zoneOne);
+            const arrZoneTwo = this.getZoneArray(zoneTwo);
 
-            const arrZoneOne = Array(zoneOneEnd - zoneOneStart + 1).fill(0)
-                .map((value, index) => value = zoneOneStart + index);
-            const arrZoneTwo = Array(zoneTwoEnd - zoneTwoStart + 1).fill(0)
-                .map((value, index) => value = zoneTwoStart + index);
+            const overlapsFullZoneOne = arrZoneOne.every(id => arrZoneTwo.includes(id));
+            const overlapsFullZoneTwo = arrZoneTwo.every(id => arrZoneOne.includes(id));
 
-            const containsZoneOneFullRange = arrZoneOne.every(id => arrZoneTwo.includes(id));
-            const containsZoneTwoFullRange = arrZoneTwo.every(id => arrZoneOne.includes(id));
-            console.log(zones, '\n', arrZoneOne, '\n', arrZoneTwo, '\n', containsZoneOneFullRange, containsZoneTwoFullRange, '\n');
-
-            if (containsZoneOneFullRange || containsZoneTwoFullRange) sumFullRanges++;
+            if (overlapsFullZoneOne || overlapsFullZoneTwo) sumFullOverlappingRanges++;
         });
 
-        return sumFullRanges.toString();
+        return sumFullOverlappingRanges.toString();
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const zonePairs: string[] = input.split('\n');
+        let sumOverlappingRanges = 0;
+
+        zonePairs.forEach(zones => {
+            const [zoneOne, zoneTwo] = zones.split(',');
+            const arrZoneOne = this.getZoneArray(zoneOne);
+            const arrZoneTwo = this.getZoneArray(zoneTwo);
+
+            const overlapsZoneOne = arrZoneOne.some(id => arrZoneTwo.includes(id));
+            const overlapsZoneTwo = arrZoneTwo.some(id => arrZoneOne.includes(id));
+
+            if (overlapsZoneOne || overlapsZoneTwo) sumOverlappingRanges++;
+        });
+
+        return sumOverlappingRanges.toString();
+    }
+
+    getZoneArray(ids: string): number[] {
+        const [start, end] = ids.split('-').map(id => Number(id));
+        return Array(end - start + 1).fill(0).map((value, index) => value = start + index);
     }
 }
 
