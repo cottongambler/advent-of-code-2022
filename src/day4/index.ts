86498c2d93ede90ca8509c38719310f9c1ed086c
Cotton <gamblercotton@gmail.com>
2022-12-04 14:58:44 +0100

    Day 4 part 1 completed

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -7,7 +7,27 @@ class Day4 extends Day {
     }
 
     solveForPartOne(input: string): string {
-        return input;
+        const zonePairs: string[] = input.split('\n');
+        let sumFullRanges = 0;
+
+        zonePairs.forEach(zones => {
+            const [zoneOne, zoneTwo] = zones.split(',');
+            const [zoneOneStart, zoneOneEnd] = zoneOne.split('-').map(id => Number(id));
+            const [zoneTwoStart, zoneTwoEnd] = zoneTwo.split('-').map(id => Number(id));
+
+            const arrZoneOne = Array(zoneOneEnd - zoneOneStart + 1).fill(0)
+                .map((value, index) => value = zoneOneStart + index);
+            const arrZoneTwo = Array(zoneTwoEnd - zoneTwoStart + 1).fill(0)
+                .map((value, index) => value = zoneTwoStart + index);
+
+            const containsZoneOneFullRange = arrZoneOne.every(id => arrZoneTwo.includes(id));
+            const containsZoneTwoFullRange = arrZoneTwo.every(id => arrZoneOne.includes(id));
+            console.log(zones, '\n', arrZoneOne, '\n', arrZoneTwo, '\n', containsZoneOneFullRange, containsZoneTwoFullRange, '\n');
+
+            if (containsZoneOneFullRange || containsZoneTwoFullRange) sumFullRanges++;
+        });
+
+        return sumFullRanges.toString();
     }
 
     solveForPartTwo(input: string): string {
