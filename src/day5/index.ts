204a6f120a61551fff3e3071602ec989445424a7
Cotton <gamblercotton@gmail.com>
2022-12-05 19:48:52 +0100

    Day 5 part 2 completed

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -3,10 +3,16 @@ import { Day } from "../day";
 type Stack = string[];
 
 const NUMBER_OF_STACKS = 9;
+
 const CHARS_PER_CRATE = 3;
 const CHARS_BETWEEN_CRATES = 1;
 const SUM_CHAR_CRATES = CHARS_PER_CRATE + CHARS_BETWEEN_CRATES;
 const LINES_BETWEEN_CRATES_AND_COMMANDS = 2;
+
+const STACK_FROM_INDEX = 3;
+const STACK_TO_INDEX = 5;
+const STACK_TIMES_INDEX = 1;
+const STACK_QUANTITIES_INDEX = 1;
 
 class Day5 extends Day {
     stacks: Stack[] = [];
@@ -17,15 +23,15 @@ class Day5 extends Day {
 
     solveForPartOne(input: string): string {
         const lines = input.split('\n');
-        const endOfStackLinesInputIndex = this.initializeStacks(lines);
+        const endOfStackLinesIndex = this.initializeStacks(lines);
         
 
-        for (let linesIndex = endOfStackLinesInputIndex + LINES_BETWEEN_CRATES_AND_COMMANDS; linesIndex < lines.length; linesIndex++) {
+        for (let linesIndex = endOfStackLinesIndex + LINES_BETWEEN_CRATES_AND_COMMANDS; linesIndex < lines.length; linesIndex++) {
             const command: string[] = lines[linesIndex].split(' ');
             if (command[0] === 'move') {
-                const fromStack = Number(command[3]);
-                const toStack = Number(command[5]);
-                const times = Number(command[1]);
+                const fromStack = Number(command[STACK_FROM_INDEX]);
+                const toStack = Number(command[STACK_TO_INDEX]);
+                const times = Number(command[STACK_TIMES_INDEX]);
                 this.moveOneCratePerTime(fromStack, toStack, times);
             }
         }
@@ -34,17 +40,31 @@ class Day5 extends Day {
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const lines = input.split('\n');
+        const endOfStackLinesIndex = this.initializeStacks(lines);
+        
+
+        for (let linesIndex = endOfStackLinesIndex + LINES_BETWEEN_CRATES_AND_COMMANDS; linesIndex < lines.length; linesIndex++) {
+            const command: string[] = lines[linesIndex].split(' ');
+            if (command[0] === 'move') {
+                const fromStack = Number(command[STACK_FROM_INDEX]);
+                const toStack = Number(command[STACK_TO_INDEX]);
+                const quantity = Number(command[STACK_QUANTITIES_INDEX]);
+                this.moveCratesAtTheSameTime(fromStack, toStack, quantity);
+            }
+        }
+
+        return this.getTopCratesInStacks();
     }
 
     initializeStacks(input: string[]): number {
         this.stacks = Array.from(Array(NUMBER_OF_STACKS), () => []);
 
-        let endIndex = 0;
         let index = 0;
-        let isEndOfStack: boolean = false;
+        let endIndex = 0;
+        let isEndOfStackLines: boolean = false;
 
-        for (index = 0; index < input.length && !isEndOfStack; index++) {
+        for (index = 0; index < input.length && !isEndOfStackLines; index++) {
             const cratesLine: string = input[index];
 
             for(let cratesLineIndex = 0; cratesLineIndex < cratesLine.length; cratesLineIndex += SUM_CHAR_CRATES) {
@@ -54,7 +74,7 @@ class Day5 extends Day {
                 }
             }
 
-            if (input[index + 1].charAt(1) === '1') isEndOfStack = true;
+            if (input[index + 1].charAt(1) === '1') isEndOfStackLines = true;
 
             endIndex++;
         }
@@ -63,10 +83,22 @@ class Day5 extends Day {
     }
 
     moveOneCratePerTime(fromStack: number, toStack: number, times: number): void {
-        console.log(fromStack, toStack, times);
         for(let i = 0; i < times; i++) {
-            const crate = this.stacks[fromStack - 1].shift() ?? '';
-            this.stacks[toStack - 1].unshift(crate);
+            const crate = this.stacks[fromStack - 1].shift();
+            if (crate) this.stacks[toStack - 1].unshift(crate);
+        }
+    }
+
+    moveCratesAtTheSameTime(fromStack: number, toStack: number, quantity: number): void {
+        const crates: string[] = [];
+
+        for(let i = 0; i < quantity; i++) {
+            const crate = this.stacks[fromStack - 1].shift();
+            if (crate) crates.push(crate);
+        }
+        for(let i = 0; i < quantity; i++) {
+            const crate = crates.pop();
+            if (crate) this.stacks[toStack - 1].unshift(crate);
         }
     }
 
