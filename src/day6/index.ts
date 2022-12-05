565f6c6d2d26cbeb27de2a94d4cd53424c48ec17
Cotton <gamblercotton@gmail.com>
2022-12-05 19:51:25 +0100

    Prep for day 6

---
+++ src/day6/index.ts
@@ -0,0 +1,18 @@
+import { Day } from "../day";
+
+class Day6 extends Day {
+
+    constructor(){
+        super(6);
+    }
+
+    solveForPartOne(input: string): string {
+        return input;
+    }
+
+    solveForPartTwo(input: string): string {
+        return input;
+    }
+}
+
+export default new Day6;
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import day2 from './day2';
 import day3 from './day3';
 import day4 from './day4';
 import day5 from './day5';
+import day6 from './day6';
 // MORE IMPORTS HERE
 
 const days: Day[] = [
@@ -13,7 +14,8 @@ const days: Day[] = [
     day2,
     day3,
     day4,
-    day5
+    day5,
+    day6
     // MORE DAYS HERE
 ];
 
