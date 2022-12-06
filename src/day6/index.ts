03f8f8171edd24f33d93a4827c472c3c500c8266
Cotton <gamblercotton@gmail.com>
2022-12-06 14:41:51 +0100

    Day 6 part 1 completed

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,5 +1,7 @@
 import { Day } from "../day";
 
+const PACKET_MARKER_SIZE = 4;
+
 class Day6 extends Day {
 
     constructor(){
@@ -7,7 +9,18 @@ class Day6 extends Day {
     }
 
     solveForPartOne(input: string): string {
-        return input;
+        const dataStream: string[] = input.split('');
+        let packetMarker: string[] = [];
+        let firstMarkerIndex = 0;
+
+        for (let i = 0; i < dataStream.length && packetMarker.length < PACKET_MARKER_SIZE; i++) {
+            if (packetMarker.includes(dataStream[i])) packetMarker = [];
+            packetMarker.push(dataStream[i]);
+
+            firstMarkerIndex = i + 1;
+        }
+
+        return firstMarkerIndex.toString();
     }
 
     solveForPartTwo(input: string): string {
