90f89e2192f69fe7522512be5651983e76877baa
Cotton <gamblercotton@gmail.com>
2022-12-06 15:41:20 +0100

    Day 6 part 2 completed

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,6 +1,9 @@
 import { Day } from "../day";
 
 const PACKET_MARKER_SIZE = 4;
+const MESSAGE_MARKER_SIZE = 14;
+
+const INDEX_NOT_FOUND = -1;
 
 class Day6 extends Day {
 
@@ -9,22 +12,28 @@ class Day6 extends Day {
     }
 
     solveForPartOne(input: string): string {
+        return this.getFirstMarkerIndex(input, PACKET_MARKER_SIZE).toString();
+    }
+
+    solveForPartTwo(input: string): string {
+        return this.getFirstMarkerIndex(input, MESSAGE_MARKER_SIZE).toString();
+    }
+
+    getFirstMarkerIndex(input: string, markerSize: number): number {
         const dataStream: string[] = input.split('');
-        let packetMarker: string[] = [];
+        let marker: string[] = [];
         let firstMarkerIndex = 0;
 
-        for (let i = 0; i < dataStream.length && packetMarker.length < PACKET_MARKER_SIZE; i++) {
-            if (packetMarker.includes(dataStream[i])) packetMarker = [];
-            packetMarker.push(dataStream[i]);
+        for (let i = 0; i < dataStream.length && marker.length < markerSize; i++) {
+            const findIndex = marker.findIndex(char => char === dataStream[i]);
+
+            if (findIndex !== INDEX_NOT_FOUND) marker = marker.slice(findIndex + 1);
+            marker.push(dataStream[i]);
 
             firstMarkerIndex = i + 1;
         }
 
-        return firstMarkerIndex.toString();
-    }
-
-    solveForPartTwo(input: string): string {
-        return input;
+        return firstMarkerIndex;
     }
 }
 
