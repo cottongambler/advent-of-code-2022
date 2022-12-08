bea8b33d4d00b4c5505d03bfd523a29e25d248fd
Laho <gamblercotton@gmail.com>
2022-12-08 18:36:55 +0100

    Prep for day 8

---
+++ src/day8/index.ts
@@ -0,0 +1,18 @@
+import { Day } from "../day";
+
+class Day8 extends Day {
+
+    constructor(){
+        super(8);
+    }
+
+    solveForPartOne(input: string): string {
+        return input;
+    }
+
+    solveForPartTwo(input: string): string {
+        return input;
+    }
+}
+
+export default new Day8;
