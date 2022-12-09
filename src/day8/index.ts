a82ddbd9432e26ae102e2e5c6a8fb4967b2dc473
Laho <gamblercotton@gmail.com>
2022-12-09 17:19:25 +0100

    Day 8 part 1 completed

--- src/day8/index.ts
+++ src/day8/index.ts
@@ -1,5 +1,12 @@
 import { Day } from "../day";
 
+type Tree = {
+    height: number;
+    visibility: boolean;
+}
+
+type Forest = Tree[][];
+
 class Day8 extends Day {
 
     constructor(){
@@ -7,11 +14,121 @@ class Day8 extends Day {
     }
 
     solveForPartOne(input: string): string {
-        return input;
+        const forest = this.initForest(input);
+        this.calculateForestVisibility(forest);
+        return this.getTotalVisibleTrees(forest).toString();
     }
 
     solveForPartTwo(input: string): string {
         return input;
+    }
+
+    initForest(input: string): Forest {
+        const lines = input.split('\n');
+        const forest: Forest = Array.from(Array(lines.length), () => []);
+        lines.forEach((line, index) => {
+            const treeLine = line.trim().split('');
+            treeLine.forEach(tree => forest[index].push({height: Number(tree), visibility: true}));
+        });
+
+        return forest;
+    }
+
+    calculateForestVisibility(forest: Forest) {
+        for(let x = 1; x < forest[0].length - 1; x++) {
+            for(let y = 1; y < forest.length - 1; y++) {
+                this.calculateTreeVisibility(forest, x, y);
+            }            
+        }
+    }
+
+    calculateTreeVisibility(forest: Forest, x: number, y: number) {
+        const tree = forest[x][y];
+
+        const leftTrees = this.getLeftTrees(forest, x, y).map(tree => tree.height);
+        const rightTrees = this.getRightTrees(forest, x, y).map(tree => tree.height);
+        const topTrees = this.getTopTrees(forest, x, y).map(tree => tree.height);
+        const bottomTrees = this.getBottomTrees(forest, x, y).map(tree => tree.height);
+
+        const leftMaxHeight = Math.max(...leftTrees);
+        const rightMaxHeight = Math.max(...rightTrees);
+        const topMaxHeight = Math.max(...topTrees);
+        const bottomMaxHeight = Math.max(...bottomTrees);
+
+        if( leftMaxHeight >= tree.height &&
+            rightMaxHeight >= tree.height &&
+            topMaxHeight >= tree.height &&
+            bottomMaxHeight >= tree.height
+            ) {
+                tree.visibility = false;
+            }
+    }
+
+    getTotalVisibleTrees(forest: Forest): number {
+        let visibleTrees = 0;
+
+        for(let x = 0; x < forest[0].length; x++) {
+            for(let y = 0; y < forest.length; y++) {
+                if(forest[x][y].visibility) visibleTrees++;
+            }
+        }
+
+        return visibleTrees;
+    }
+
+    getLeftTrees(forest: Forest, x: number, y: number) : Tree[] {
+        const trees: Tree[] = [];
+        for(let i = 0; i < x; i++) {
+            trees.push(forest[i][y]);
+        }
+        return trees;
+    }
+
+    getRightTrees(forest: Forest, x: number, y: number) : Tree[] {
+        const trees: Tree[] = [];
+        for(let i = x + 1; i < forest[x].length; i++) {
+            trees.push(forest[i][y]);
+        }
+        return trees;
+    }
+
+    getTopTrees(forest: Forest, x: number, y: number) : Tree[] {
+        const trees: Tree[] = [];
+        for(let i = 0; i < y; i++) {
+            trees.push(forest[x][i]);
+        }
+        return trees;
+    }
+
+    getBottomTrees(forest: Forest, x: number, y: number) : Tree[] {
+        const trees: Tree[] = [];
+        for(let i = y + 1; i < forest.length ; i++) {
+            trees.push(forest[x][i]);
+        }
+        return trees;
+    }
+
+    printForest(forest: Forest) {
+        let line = '';
+
+        for(let x = 0; x < forest[0].length; x++) {
+            for(let y = 0; y < forest.length; y++) {
+                line += (forest[x][y].height).toString() + ' ';
+            }
+            console.log(line, '\n');
+            line = '';
+        }
+
+        console.log('\n');
+        
+
+        for(let x = 0; x < forest[0].length; x++) {
+            for(let y = 0; y < forest.length; y++) {
+                line += (forest[x][y].visibility ? 1 : 0).toString() + ' ';
+            }
+            console.log(line, '\n');
+            line = '';
+        }
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import day4 from './day4';
 import day5 from './day5';
 import day6 from './day6';
 import day7 from './day7';
+import day8 from './day8';
 // MORE IMPORTS HERE
 
 const days: Day[] = [
@@ -17,7 +18,8 @@ const days: Day[] = [
     day4,
     day5,
     day6,
-    day7
+    day7,
+    day8
     // MORE DAYS HERE
 ];
 
