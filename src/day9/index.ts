50272d90dfd1736ae5f395cbb7e764d93512acff
Laho <gamblercotton@gmail.com>
2022-12-10 15:36:43 +0100

    Prep for day 9

---
+++ src/day9/index.ts
@@ -0,0 +1,18 @@
+import { Day } from "../day";
+
+class Day9 extends Day {
+
+    constructor(){
+        super(9);
+    }
+
+    solveForPartOne(input: string): string {
+        return input;
+    }
+
+    solveForPartTwo(input: string): string {
+        return input;
+    }
+}
+
+export default new Day9;
